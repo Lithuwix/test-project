cd598da074aa51bed772ac9aa944a114ac5fa233
Vova Traihel <vovatraigel@gmail.com>
2022-06-26 12:06:22 +0300

    oChangeHandler

--- src/components/Main/Test_component_02/Test_component_02.tsx
+++ src/components/Main/Test_component_02/Test_component_02.tsx
@@ -1,4 +1,5 @@
-import React, {useState} from 'react';
+import React, {ChangeEvent, useState} from 'react';
+import { v1 } from 'uuid';
 
 type NameArrType = {
     id: string
@@ -8,10 +9,18 @@ type NameArrType = {
 export const TestComponent02 = () => {
 
     const [nameArr, setNameArr] = useState<Array<NameArrType>>([])
+    const [valueName, setValueName] = useState<string>('')
+
+    const oChangeHandler = (e: ChangeEvent<HTMLInputElement>) => setValueName(e.currentTarget.value)
+
     return (
         <div>
-            <input/>
+            <input value={valueName} onChange={oChangeHandler}/>
             <button>New name</button>
+            <ul>
+                {nameArr.map(el => <li>{el.name}</li>)}
+            </ul>
+
         </div>
     );
 };
